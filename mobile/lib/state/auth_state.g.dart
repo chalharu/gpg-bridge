153deace347b6380d34c73867b3a6fccadeb21a2
chalharu <1978012+chalharu@users.noreply.github.com>
2026-02-23 06:40:09 +0900

    chore(mobile): upgrade riverpod and go_router deps

--- mobile/lib/state/auth_state.g.dart
+++ mobile/lib/state/auth_state.g.dart
@@ -6,20 +6,57 @@ part of 'auth_state.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(AuthState)
+const authStateProvider = AuthStateProvider._();
+
+final class AuthStateProvider extends $NotifierProvider<AuthState, bool> {
+  const AuthStateProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'authStateProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$authStateHash();
+
+  @$internal
+  @override
+  AuthState create() => AuthState();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(bool value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<bool>(value),
+    );
+  }
+}
+
 String _$authStateHash() => r'2f3be601a4cce3e6706b7a8c90afec1348b8b619';
 
-/// See also [AuthState].
-@ProviderFor(AuthState)
-final authStateProvider = AutoDisposeNotifierProvider<AuthState, bool>.internal(
-  AuthState.new,
-  name: r'authStateProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$authStateHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef _$AuthState = AutoDisposeNotifier<bool>;
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+abstract class _$AuthState extends $Notifier<bool> {
+  bool build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref = this.ref as $Ref<bool, bool>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<bool, bool>,
+              bool,
+              Object?,
+              Object?
+            >;
+    element.handleValue(ref, created);
+  }
+}
